31586f9f90494b13ef0783c9a91a5f5fa6c5313b
Pallavi Das K <47100069+pallavi-das-k@users.noreply.github.com>
2024-02-29 20:39:37 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -438,7 +438,7 @@ def build_targets(p, targets, model):
             # r = t[None, :, 4:6] / anchors[:, None]  # wh ratio
             # j = torch.max(r, 1. / r).max(2)[0] < model.hyp['anchor_t']  # compare
             j = wh_iou(anchors, t[:, 4:6]) > model.hyp['iou_t']  # iou(3,n) = wh_iou(anchors(3,2), gwh(n,2))
-            a, t = at[j], t.repeat(na, 1, 1)[j]  # filter
+            a, t = at[j.item()], t.repeat(na, 1, 1)[j.item()]  # filter
 
             # overlaps
             gxy = t[:, 2:4]  # grid xy
