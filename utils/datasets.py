c33f6bcc61608dd6e299fd85675c3bc9f01573d0
Pallavi Das K <47100069+pallavi-das-k@users.noreply.github.com>
2024-02-29 19:52:48 +0530

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -270,7 +270,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
                 f = glob.iglob(path + os.sep + '*.*')
             else:
                 raise Exception('%s does not exist' % path)
-            self.img_files = [x.replace('/', os.sep) for x in f if os.path.splitext(x)[-1].lower() in img_formats]
+            self.img_files = ["/kaggle/input/animal/train/" + x.replace('/', os.sep) for x in f if os.path.splitext(x)[-1].lower() in img_formats]
         except:
             raise Exception('Error loading data from %s. See %s' % (path, help_url))
 
